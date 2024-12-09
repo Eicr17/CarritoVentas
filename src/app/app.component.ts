102d168bf760a5836122f959671b8e43aea84758
Raúl Canel <rcanel@agexport.org.gt>
2024-12-09 15:29:48 -0600

    Se agrega get  de productos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SrvProductosService } from './Servicios/srv-productos.service';
+import { MdlProducto } from './modelos/mdl-Producto.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'IndCarritoCompras';
+  lstProductos: MdlProducto[] = [];
+  
+  constructor(private srvProductos : SrvProductosService){
+    
+    this.srvProductos.obtenerProductos().subscribe(
+      (response) => {
+          //console.log(response);
+          this.lstProductos = response.datos;
+          //console.log(this.lstProductos[0]);
+          //console.log(this.lstProductos[0].id_producto);
+          //console.log(this.lstProductos);
+      },
+      (fail : any) =>{
+          console.log(fail);
+      }
+    )
+  }
+
+
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    api_carrito: "http://localhost:5262/api",
+  };
+  
