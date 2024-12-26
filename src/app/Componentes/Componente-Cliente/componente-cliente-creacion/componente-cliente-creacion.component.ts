6b19131cb591efac919751f0282f634ae5205b45
Eicr17 <eisraelcv20@gmail.com>
2024-12-26 15:01:44 -0600

    Modificacion de componentes

--- src/app/Componentes/Componente-Cliente/componente-cliente-creacion/componente-cliente-creacion.component.ts
+++ src/app/Componentes/Componente-Cliente/componente-cliente-creacion/componente-cliente-creacion.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MdlClienteCreacion } from 'src/app/modelos/mdl-cliente-creacion.model';
 import { MdlCliente } from 'src/app/modelos/mdl-cliente.model';
 import { SrvClienteService } from 'src/app/Servicios/srv-cliente.service';
@@ -16,7 +16,26 @@ public formClientes! : FormGroup
 constructor(private srvClientes: SrvClienteService,
   private formBuilider : FormBuilder){
 
-
-
-}
+  }
+  NgOnInit(){
+    this.formClientes = this.formBuilider.group({
+      nombres : ['', Validators.required],
+      apellidos : ['', Validators.required],
+      dpi : ['', Validators.required],
+      telefono : ['', Validators.required],
+    })
+  }
+  GrabarCliente(){
+    this.srvClientes.CrearCliente(
+      this.formClientes.value
+    ).subscribe(
+      (response) => {
+        if (response.codigooperacion == 0)
+          alert("Cliente creado exitosamente")
+      },
+      (fail : any) =>{
+        console.log(fail);
+      }
+    )
+  }
 }
